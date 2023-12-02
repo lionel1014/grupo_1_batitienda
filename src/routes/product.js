21f683c72e61d92fb0e66c505026438182c864c0
Lionel Diez Gomez <satur.1512@gmail.com>
2023-12-02 10:32:49 -0300

    configuracion de los middlewares

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const multer = require('multer');
+
 const productController = require("../controllers/productController");
 
+const storage = multer.diskStorage({
+	destination: (req,file,cd) => {
+		cd(null, path.join(__dirname, '../public/images/img_products'))
+	},
+	filename: (req,file,cd) =>{
+        console.log(file);
+		const uniqueSuffix = Date.now();
+		const fileExtension = path.extname(file.originalname);
+		cd(null, `${file.fieldname}-${uniqueSuffix}${fileExtension}`);
+	}
+})
+
+const uploadFile = multer({storage});
 const router = express.Router();
+
+// router.post('/', upload.single('form-image'), productsController.store);  el parametro de .single es el name del input type file
 
 router.get("/productCar", productController.productCar);
 router.get("/createProduct", productController.createProduct);
