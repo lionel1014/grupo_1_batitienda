eace8ab90483c32838eb5c1e7ba1e6af20378c45
Lionel Diez Gomez <satur.1512@gmail.com>
2023-12-20 21:34:37 -0300

    intento de validator

---
+++ src/middleware/validateProduct.js
@@ -0,0 +1,16 @@
+const { check, param, validationResult, body} = require('express-validator');
+const Product = require("../models/Product")
+
+const validateProduct = [
+    body("titulo").notEmpty().withMessage("Debe tener un nombre el producto").bail()
+    .isLength({min: 3}).withMessage("Debe ser mayor a 3 caracteres").bail()
+    .custom( titulo => {
+        const isTitleInUse = Product.getAllProducts().some( product => product.title == titulo)
+        console.log("llego?")
+        if (isTitleInUse) {
+            throw new Error("Ya existe un producto con ese titulo")
+        }
+    })
+];
+
+module.exports = validateProduct;
