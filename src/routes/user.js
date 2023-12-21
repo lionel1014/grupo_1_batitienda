8517d3aafb6799643985c15860946034a572a9e2
Esteban-Frean <estebanfrean@gmail.com>
2023-12-21 13:46:35 -0300

    Validacion de Registro completa

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const multer = require("multer");
 const PATH = require('path');
+const { check } = require("express-validator")
 const userController = require("../controllers/userController");
 const guessMiddleware = require("../middleware/guessMiddleware");
 const authMiddleware = require("../middleware/authmiddleware");
 const updateCookieMiddleware = require("../middleware/updateCookieMiddleware")
+const router = express.Router();
 
 const storage = multer.diskStorage({
 	destination: (req,file,cd) => {
@@ -17,13 +19,43 @@ const storage = multer.diskStorage({
 	}
 });
 
-const router = express.Router();
 const uploadFile = multer({storage});
+
+let validateRegisterForm = [
+	uploadFile.single("imagen"),
+	check('imagen')
+    .custom((value, { req }) => {
+      if (!req.file) {
+        throw new Error('Debes seleccionar una imagen');
+      }
+      return true;
+    }),
+	check("nombre")
+		.notEmpty().withMessage("Debes completar el nombre"),
+	check("apellido")
+		.notEmpty().withMessage("Debes completar el apellido"),
+	check("correo")
+		.notEmpty().withMessage("Debes completar el correo").bail()
+		.normalizeEmail()
+		.isEmail().withMessage("Debes ingresar un correo valido"),
+	check("usuario")
+		.notEmpty().withMessage("Debes completar el nombre de usuario").bail()
+		.isLength({min : 5}).withMessage("El nombre de usuario debe tener minimo 5 caracteres"),
+	check("contrasena")
+		.notEmpty().withMessage("Debes completar la contraseña").bail()
+		.isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
+		.matches(/^(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]).*$/)
+		.withMessage('La contraseña debe contener al menos una letra mayúscula y un símbolo'),
+	check("confirmar_contrasena")
+		.notEmpty().withMessage("Debes confirmar la contraseña").bail()
+		.equals('contrasena').withMessage('Las contraseñas no coinciden')
+]
+
 
 router.get("/", guessMiddleware, userController.register);
 router.get("/login", guessMiddleware, userController.login);
 router.get("/profile", authMiddleware, userController.profile)
-router.post("/", uploadFile.single("imagen") ,userController.createProcess);
+router.post("/", validateRegisterForm, userController.createProcess);
 router.post("/login", userController.loginProcess);
 router.post("/logout", userController.logout);
 router.put("/img/:id",  uploadFile.single("img-usuario"), updateCookieMiddleware, userController.changeUserImage)
